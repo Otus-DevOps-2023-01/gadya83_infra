8fe62eb4908d0b8aae5c83eaa2d7f221a045fa36
gadya83 <vadim.lapshin.83@gmail.com>
2023-03-12 14:18:35 +0300

    add cloud-testapp

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+sudo apt update
+sudo apt install mongodb -y
+systemctl -q is-active mongodb && echo "$SERVICE mongodb was active"
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+sudo systemctl enable mongod
 if (git --version | grep 'command not found' )
 then
 sudo apt install git-all
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,3 +4,4 @@ sudo apt install mongodb -y
 systemctl -q is-active mongodb && echo "$SERVICE mongodb was active"
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
+sudo systemctl enable mongod
