ad4333ccd87a48507244470d3e324e47b09db118
gadya83 <vadim.lapshin.83@gmail.com>
2023-03-12 14:46:31 +0300

    add cloud-testapp

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-chmod u+x ./install_ruby.sh
-./install_ruby.sh
-chmod u+x ./install_mongodb.sh
-./install_mongodb.sh
 if (git --version | grep 'command not found' )
 then
 sudo apt install git-all
