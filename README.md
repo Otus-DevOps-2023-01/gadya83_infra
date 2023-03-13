76f2d46c4cf560b21e5b0d795af83d0bb98556a6
gadya83 <vadim.lapshin.83@gmail.com>
2023-03-13 20:13:35 +0300

    fix testapp_IP

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ bastion_IP = 158.160.22.130
 someinternalhost_IP = 10.129.0.13
 ssh -J bastion_IP someinternalhost_IP
 
-testapp_IP = 158.160.17.243
+testapp_IP = 51.250.104.140
 testapp_port = 9292
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,4 +4,3 @@ sudo apt install mongodb -y
 systemctl -q is-active mongodb && echo "$SERVICE mongodb was active"
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
-sudo systemctl enable mongod
